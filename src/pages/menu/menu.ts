16267b64b11dac03d2f18339d0acef2e36dcd4f0
Eli Charashvili <elenikoguda@gmail.com>
2026-03-17 22:24:37 +0400

    new page by eli

---
+++ src/pages/menu/menu.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  imports: [],
+  templateUrl: './menu.html',
+  styleUrl: './menu.css',
+})
+export class Menu {}
